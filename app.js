80743261989c46d407b9b033432e5103818c4162
Alexis Green <jalexis@jalexis.net>
2017-04-03 14:14:39 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+function getTokens(rawString) {
+  // NB: `.filter(Boolean)` removes any falsy items from an array
+  return rawString.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+}
+  
+function mostFrequentWord(text) {
+  var words = getTokens(text);
+  var wordFrequencies = {};
+  for (var i = 0; i <= words.length; i++) {
+    if (words[i] in wordFrequencies) {
+      wordFrequencies[words[i]]++;
+    }
+    else {
+      wordFrequencies[words[i]]=1;
+    }
+    console.log(wordFrequencies);
+  }
+  var currentMaxKey = Object.keys(wordFrequencies)[0];
+  var currentMaxCount = wordFrequencies[currentMaxKey];
+  
+  for (var word in wordFrequencies) {
+    if (wordFrequencies[word] > currentMaxCount) {
+      currentMaxKey = word;
+      currentMaxCount = wordFrequencies[word];
+    }
+  }
+  return currentMaxKey;
+}
+
+var answer = mostFrequentWord("the quick brown fox jumps over the lazy dogs");
+console.log(answer);
